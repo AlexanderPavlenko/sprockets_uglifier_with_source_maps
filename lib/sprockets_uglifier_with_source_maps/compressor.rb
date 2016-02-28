253dd32f74664bec4656c4080b57e38275d2b3e5
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2016-02-28 18:04:17 +0300

    v2.0.0

--- lib/sprockets_uglifier_with_source_maps/compressor.rb
+++ lib/sprockets_uglifier_with_source_maps/compressor.rb
@@ -4,37 +4,42 @@ require 'sprockets/uglifier_compressor'
 module SprocketsUglifierWithSM
   class Compressor < Sprockets::UglifierCompressor
 
+    DEFAULTS = { comments: false }
+
     def initialize(options = {})
       # merge in any options passed in from our rails configuration - i wish
       # rails actually did this by default :/
-      options = {comments: false}.merge!(Rails.application.config.assets.uglifier.to_h)
+      options = options.merge(DEFAULTS).merge!(Rails.application.config.assets.uglifier.to_h)
       super options
     end
 
     def call(input)
-      compressed_data, sourcemap = @uglifier.compile_with_map(input[:data])
+      data = input.fetch(:data)
+      name = input.fetch(:name)
 
-      uncompressed_filename = File.join(Rails.application.config.assets.prefix, Rails.application.config.assets.uncompressed_prefix, "#{input[:name]}-#{digest(input[:data])}.js")
-      uncompressed_url = filename_to_url uncompressed_filename
+      compressed_data, sourcemap = @uglifier.compile_with_map(data)
 
-      sourcemap = JSON.parse(sourcemap)
-      sourcemap['file'] = "#{input[:name]}.js"
+      uncompressed_filename = File.join(Rails.application.config.assets.prefix, Rails.application.config.assets.uncompressed_prefix, "#{name}-#{digest(data)}.js")
+      uncompressed_url      = filename_to_url uncompressed_filename
+
+      sourcemap            = JSON.parse(sourcemap)
+      sourcemap['file']    = "#{name}.js"
       sourcemap['sources'] = [uncompressed_url]
 
       sourcemap = sourcemap.to_json
 
-      sourcemap_filename = File.join(Rails.application.config.assets.prefix, Rails.application.config.assets.sourcemaps_prefix, "#{input[:name]}-#{digest(sourcemap)}.js.map")
+      sourcemap_filename = File.join(Rails.application.config.assets.prefix, Rails.application.config.assets.sourcemaps_prefix, "#{name}-#{digest(sourcemap)}.js.map")
 
-      sourcemap_path = File.join(Rails.public_path, sourcemap_filename)
+      sourcemap_path    = File.join(Rails.public_path, sourcemap_filename)
       uncompressed_path = File.join(Rails.public_path, uncompressed_filename)
       FileUtils.mkdir_p [File.dirname(sourcemap_path), File.dirname(uncompressed_path)]
       File.open(sourcemap_path, 'w') { |f| f.write sourcemap }
-      File.open(uncompressed_path, 'w') { |f| f.write input[:data] }
+      File.open(uncompressed_path, 'w') { |f| f.write data }
 
       if Rails.application.config.assets.sourcemaps_gzip
-        [sourcemap_path,uncompressed_path ].each do |f|
+        [sourcemap_path, uncompressed_path].each do |f|
           Zlib::GzipWriter.open("#{f}.gz") do |gz|
-            gz.mtime = File.mtime(f)
+            gz.mtime     = File.mtime(f)
             gz.orig_name = f
             gz.write IO.binread(f)
           end
